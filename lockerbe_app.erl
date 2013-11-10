e429c9b10f8093e90def7935e2dfc42c2102b011
Ales Bublik <abublik@redhat.com>
2013-11-10 08:49:17 +0100

    new handler

--- lockerbe_app.erl
+++ lockerbe_app.erl
@@ -5,8 +5,16 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        %% {URIHost, list({URIPath, Handler, Opts})}
+        {'_', [{'_', lockerbe_handler, []}]}
+    ]),
+    %% Name, NbAcceptors, TransOpts, ProtoOpts
+    cowboy:start_http(my_http_listener, 100,
+        [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
     lockerbe_sup:start_link().
-
 
 stop(_State) ->
     ok.
